b5b3079f325f64b2ad684ed1d1926fb846e2b84c
Guilherme Rodrigues <gadr90@gmail.com>
2022-02-07 14:14:04 -0300

    New strategy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { join } = require('path');
 const { existsSync } = require('fs');
-const { renderApp } = require('./build/index.server.js');
-const symbols = require('./build/q-symbols.json');
+const { render } = require('./build/entry.server.js');
+const symbols = require('./q-symbols.json');
 const PORT = process.env.PORT || 8080;
 
 async function startServer() {
   async function handleQwik(req, res) {
-    const result = await renderApp({
+    const result = await render({
       symbols,
       url: new URL(`${req.protocol}://${req.hostname}${req.url}`),
       debug: true,
@@ -18,12 +18,6 @@ async function startServer() {
   const app = express();
   const publicDir = join(__dirname, '..', 'public');
   app.use(express.static(publicDir));
-
-  // Optionally server Partytown if found.
-  const partytownDir = join(__dirname, '..', 'node_modules', '@builder.io', 'partytown', 'lib');
-  if (existsSync(partytownDir)) {
-    app.use('/~partytown', express.static(partytownDir));
-  }
 
   app.get('/', handleQwik);
 
---
+++ server/server.ts
@@ -0,0 +1,27 @@
+import { serve } from 'https://deno.land/std@0.116.0/http/server.ts'
+
+import { render } from './build/entry.server.js';
+import symbols  from './q-symbols.json' assert {type: "json"};
+
+const addr = ':8080'
+
+async function renderApp(request: Request) {
+    const result = await render({
+      symbols,
+      url: new URL(request.url),
+      debug: true,
+    });
+    return result;
+  }
+
+async function handler (request: Request): Promise<Response> {
+  if (request.url === 'http://localhost:8080/') {
+    const result = await renderApp(request)
+    return new Response(result.html, { headers: { 'Content-Type': 'text/html' } })
+  }
+
+  return new Response('404', { status: 404 })
+}
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`)
+await serve(handler, { addr })
